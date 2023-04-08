e9bd11729f6657366a647ecdd6d6fc21f8a2f901
Christian Weaver <sparc399@gmail.com>
2023-04-08 07:54:48 -0500

    filled out more of the control sequences

--- src/net/forevercoding/consolefun/ColouredSystemOutPrintln.java
+++ src/net/forevercoding/consolefun/ColouredSystemOutPrintln.java
@@ -1,8 +1,19 @@
 package net.forevercoding.consolefun;
 
 public class ColouredSystemOutPrintln {
+    //region ANSI Control
     public static final String ANSI_RESET  = "\u001B[0m";
     public static final String ANSI_CLS    = "\u001B[2J";
+    public static final String ANSI_HOME   = "\u001B[H";
+    public static final String ANSI_BOLD   = "\u001B[1m";
+    public static final String ANSI_ITALIC = "\u001B[3m";
+    public static final String ANSI_UNDERLINE = "\u001B[4m";
+    public static final String ANSI_BLINK  = "\u001B[5m";
+    public static final String ANSI_REVERSE = "\u001B[7m";
+
+    //endregion
+
+    //region Foreground Colours
     public static final String ANSI_BLACK  = "\u001B[30m";
     public static final String ANSI_RED    = "\u001B[31m";
     public static final String ANSI_GREEN  = "\u001B[32m";
@@ -20,14 +31,9 @@ public class ColouredSystemOutPrintln {
     public static final String ANSI_BRIGHT_PURPLE = "\u001B[95m";
     public static final String ANSI_BRIGHT_CYAN   = "\u001B[96m";
     public static final String ANSI_BRIGHT_WHITE  = "\u001B[97m";
+    //endregion
 
-    public static final String[] FOREGROUNDS = {
-            ANSI_BLACK, ANSI_RED, ANSI_GREEN, ANSI_YELLOW,
-            ANSI_BLUE, ANSI_PURPLE, ANSI_CYAN, ANSI_WHITE,
-            ANSI_BRIGHT_BLACK, ANSI_BRIGHT_RED, ANSI_BRIGHT_GREEN, ANSI_BRIGHT_YELLOW,
-            ANSI_BRIGHT_BLUE, ANSI_BRIGHT_PURPLE, ANSI_BRIGHT_CYAN, ANSI_BRIGHT_WHITE
-    };
-
+    //region Background Colours
     public static final String ANSI_BG_BLACK  = "\u001B[40m";
     public static final String ANSI_BG_RED    = "\u001B[41m";
     public static final String ANSI_BG_GREEN  = "\u001B[42m";
@@ -45,13 +51,22 @@ public class ColouredSystemOutPrintln {
     public static final String ANSI_BRIGHT_BG_PURPLE = "\u001B[105m";
     public static final String ANSI_BRIGHT_BG_CYAN   = "\u001B[106m";
     public static final String ANSI_BRIGHT_BG_WHITE  = "\u001B[107m";
+    //endregion
+
+    //region Foreground and Background String Arrays
+    public static final String[] FOREGROUNDS = {
+            ANSI_BLACK, ANSI_RED, ANSI_GREEN, ANSI_YELLOW,
+            ANSI_BLUE, ANSI_PURPLE, ANSI_CYAN, ANSI_WHITE,
+            ANSI_BRIGHT_BLACK, ANSI_BRIGHT_RED, ANSI_BRIGHT_GREEN, ANSI_BRIGHT_YELLOW,
+            ANSI_BRIGHT_BLUE, ANSI_BRIGHT_PURPLE, ANSI_BRIGHT_CYAN, ANSI_BRIGHT_WHITE
+    };
 
     public static final String[] BACKGROUNDS = {
             ANSI_BG_BLACK, ANSI_BG_RED, ANSI_BG_GREEN, ANSI_BG_YELLOW,
             ANSI_BG_BLUE, ANSI_BG_PURPLE, ANSI_BG_CYAN, ANSI_BG_WHITE,
             ANSI_BRIGHT_BG_BLACK, ANSI_BRIGHT_BG_RED, ANSI_BRIGHT_BG_GREEN, ANSI_BRIGHT_BG_YELLOW,
             ANSI_BRIGHT_BG_BLUE, ANSI_BRIGHT_BG_PURPLE, ANSI_BRIGHT_BG_CYAN, ANSI_BRIGHT_BG_WHITE };
-
+    //endregion
 
     // NOTE: for cursor positioning to work, we need to run this from an actual console window rather than an IDE.
     // from the directory where the class resides
